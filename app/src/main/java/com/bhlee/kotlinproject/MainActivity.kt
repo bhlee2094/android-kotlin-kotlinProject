eae536f00bc9fb87c03c14eec60c7640ba000bb4
bh lee <djbh123@naver.com>
2021-04-30 18:57:51 +0900

    상단 toolbar

--- app/src/main/java/com/bhlee/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/bhlee/kotlinproject/MainActivity.kt
@@ -2,6 +2,9 @@ package com.bhlee.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +15,16 @@ class MainActivity : AppCompatActivity() {
         var fireMonster1 = FireMonster("불씨", 1, 5);
         var waterMonster1 = WaterMonster("방울", 1, 5);
         var windMonster1 = WindMonster("살랑", 1, 5);
-        fireMonster1.info()
-        waterMonster1.info()
-        windMonster1.info()
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar) // 상단 toolbar
+        setSupportActionBar(toolbar)
+        val actionbar = supportActionBar!!
+        actionbar.setDisplayShowTitleEnabled(false)
+
+        var mainTextView = findViewById<TextView>(R.id.MainTextView)
+        mainTextView.setText("몬스터 이름: " + fireMonster1.name + ", 몬스터 공격력: " + fireMonster1.damage + ", 몬스터 체력: " + fireMonster1.health + "\n")
+        mainTextView.append("몬스터 이름: " + waterMonster1.name + ", 몬스터 공격력: " + waterMonster1.damage + ", 몬스터 체력: " + waterMonster1.health + "\n")
+        mainTextView.append("몬스터 이름: " + windMonster1.name + ", 몬스터 공격력: " + windMonster1.damage + ", 몬스터 체력: " + windMonster1.health + "\n")
     }
 
 }
--- app/src/main/java/com/bhlee/kotlinproject/Monster.kt
+++ app/src/main/java/com/bhlee/kotlinproject/Monster.kt
@@ -1,11 +1,8 @@
 package com.bhlee.kotlinproject
 
-import android.util.Log
 
 open class Monster (var name:String, var damage:Int, var health:Int, var type:String){
-    fun info(){
-        Log.d("Monster", "이름: ${name}, 공격력: ${damage}, 체력: ${health}, 타입: ${type}")
-    }
+
 }
 
 class FireMonster (name:String, damage:Int, health:Int) : Monster (name, damage, health, "Fire"){
